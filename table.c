ad1ea9b992d0bfa19e2465ad8cbb371ac045ba57
flaribbit <1149761294@qq.com>
2022-09-12 21:17:45 +0800

    修复致命bug
    - 搬走时prev->next未指向新的地址
    - resize时free错了

--- table.c
+++ table.c
@@ -14,15 +14,18 @@ typedef struct Node {
 } Node;
 
 typedef struct {
-    Node *node;
-    Node *lastfree;
     size_t size;
     size_t capacity;
+    Node *node;
+    Node *lastfree;
 } Table;
 
 uint32_t Table_hash(TKey val) {
     return val;
 }
+
+Table *Table_resize(Table *this);
+void Table_debugprint(Table *this);
 
 Table *Table_new() {
     Table *this = malloc(sizeof(Table));
@@ -30,11 +33,11 @@ Table *Table_new() {
     this->size = 0;
     this->capacity = 8;
     this->node = malloc(sizeof(Node) * this->capacity);
+    this->lastfree = this->node + this->capacity - 1;
     if (!this->node) {
         printf("failed to malloc nodes");
         return NULL;
     }
-    this->lastfree = this->node + this->capacity - 1;
     for (size_t i = 0; i < this->capacity; i++) {
         this->node[i].empty = true;
     }
@@ -45,12 +48,15 @@ void Table_find_free(Table *this) {
     while (!this->lastfree->empty) {
         this->lastfree--;
         if (this->lastfree < this->node) {
-            this->lastfree = this->node + this->capacity + 1;
+            this->lastfree = this->node + this->capacity - 1;
         }
     }
 }
 
 Table *Table_add(Table *this, TKey key, TValue value) {
+    if (this->size == this->capacity) {
+        Table_resize(this);
+    }
     size_t pos = Table_hash(key) % this->capacity;
     Node *node = this->node + pos;
     // 没有冲突 直接存储
@@ -71,9 +77,9 @@ Table *Table_add(Table *this, TKey key, TValue value) {
             if (p->next == node) break;
             p = p->next;
         }  // 理论上p不是空指针
-        p->next = this->lastfree;
         // 搬走
         Table_find_free(this);
+        p->next = this->lastfree;
         *this->lastfree = *node;
         node->key = key;
         node->value = value;
@@ -115,6 +121,23 @@ Node *Table_find(Table *this, TKey key) {
 }
 
 Table *Table_resize(Table *this) {
+    Table table;
+    table.size = 0;
+    table.capacity = this->capacity * 2;
+    table.node = malloc(sizeof(Node) * table.capacity);
+    table.lastfree = table.node + table.capacity - 1;
+    for (size_t i = 0; i < this->capacity; i++) {
+        table.node[i].empty = true;
+    }
+    for (size_t i = 0; i < this->size; i++) {
+        Table_add(&table, this->node[i].key, this->node[i].value);
+    }
+    // Table_debugprint(&table);
+    free(this->node);
+    this->size = table.size;
+    this->node = table.node;
+    this->capacity = table.capacity;
+    this->lastfree = table.lastfree;
     return this;
 }
 
@@ -164,13 +187,20 @@ Table *Table_set(Table *this, TKey key, TValue value) {
 }
 
 void Table_free(Table *this) {
+    free(this->node);
     free(this);
 }
 
 void Table_debugprint(Table *this) {
     for (size_t i = 0; i < this->capacity; i++) {
         Node *node = this->node + i;
-        printf("Node[%zd](empty=%d, key=%d, value=%d, next=0x%p)\n", i, node->empty, node->key, node->value, node->next);
+        int pos = node->next - this->node;
+        if (pos < 0 || pos >= this->capacity) pos = -1;
+        if (!node->empty) {
+            printf("Node[%zd](key=%d, value=%d, next=%d)\n", i, node->key, node->value, pos);
+        } else {
+            printf("Node[%zd]()\n", i);
+        }
     }
 }
 
@@ -189,6 +219,9 @@ int main() {
     Table_set(table, 5, 114);
     Table_set(table, 6, 114);
     Table_set(table, 7, 114);
+    Table_set(table, 8, 114);
+    Table_set(table, 9, 114);
+    Table_set(table, 10, 114);
     Table_debugprint(table);
     printf("%p\n", Table_find(table, 2));
     printf("%p\n", Table_find(table, 3));
