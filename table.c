c0b846cec7774a1009006dab8700d4edeb48cdc7
flaribbit <1149761294@qq.com>
2022-09-12 16:02:41 +0800

    测试插入冲突 通过
    - TODO: 把占位的移走

--- table.c
+++ table.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef int TKey;
 typedef int TValue;
 
-typedef struct {
+typedef struct Node {
+    bool empty;
     TKey key;
     TValue value;
-    Node *next;
+    struct Node *next;
 } Node;
 
 typedef struct {
@@ -22,15 +25,90 @@ uint32_t Table_hash(TKey val) {
 }
 
 Table *Table_new() {
-    Table *table = malloc(sizeof(Table));
-    if (!table) return NULL;
-    table->size = 0;
-    table->capacity = 8;
+    Table *this = malloc(sizeof(Table));
+    if (!this) return NULL;
+    this->size = 0;
+    this->capacity = 8;
+    this->node = malloc(sizeof(Node) * this->capacity);
+    if (!this->node) {
+        printf("failed to malloc nodes");
+        return NULL;
+    }
+    this->lastfree = this->node + this->capacity - 1;
+    for (size_t i = 0; i < this->capacity; i++) {
+        this->node[i].empty = true;
+    }
+    return this;
 }
 
 Table *Table_add(Table *this, TKey key, TValue value) {
     size_t pos = Table_hash(key) % this->capacity;
+    Node *node = this->node + pos;
+    // 没有冲突 直接存储
+    if (node->empty) {
+        node->key = key;
+        node->value = value;
+        node->empty = false;
+        node->next = NULL;
+        this->size++;
+        return this;
+    }
+    // 解决冲突
+    node = this->lastfree;
+    node->key = key;
+    node->value = value;
+    node->empty = false;
+    node->next = NULL;
+    this->size++;
+    // 头插
+    Node *head = this->node + pos;
+    node->next = head->next;
+    head->next = node;
+    // 寻找空闲节点
+    while (!this->lastfree->empty) {
+        this->lastfree--;
+    }
+    return this;
+}
+
+Node *Table_find(Table *this, TKey key) {
+    size_t pos = Table_hash(key) % this->capacity;
+    Node *node = this->node + pos;
+    while (node) {
+        if (node->key == key) {
+            return node;
+        }
+        node = node->next;
+    }
+    return NULL;
 }
 
 Table *Table_set(Table *this, TKey key, TValue value) {
+    Node *node = Table_find(this, key);
+    if (node) {
+        node->value = value;
+    } else {
+        Table_add(this, key, value);
+    }
+    return this;
+}
+
+void Table_free(Table *this) {
+    free(this);
+}
+
+void Table_debugprint(Table *this) {
+    for (size_t i = 0; i < this->capacity; i++) {
+        Node *node = this->node + i;
+        printf("Node[%d](empty=%d, key=%d, value=%d, next=0x%p)\n", i, node->empty, node->key, node->value, node->next);
+    }
+}
+
+int main() {
+    Table *table = Table_new();
+    Table_set(table, 3, 114);
+    Table_set(table, 11, 514);
+    Table_set(table, 19, 514);
+    Table_debugprint(table);
+    Table_free(table);
 }
