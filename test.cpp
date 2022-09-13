ec923eecd60accede84c24b2d0952d911767d116
flaribbit <1149761294@qq.com>
2022-09-13 09:42:01 +0800

    完善测试

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ extern "C" {
 #include <unordered_map>
 #include <chrono>
 
-void test0() {
+int test0() {
     srand(114514);
     for (int i = 0; i < 20000; i++) {
         int key = rand();
@@ -13,9 +13,10 @@ void test0() {
     for (int i = 0; i < 20000; i++) {
         int key = rand();
     }
+    return 0;
 }
 
-void test1() {
+int test1() {
     Table *table = Table_new();
     srand(114514);
     int count = 0;
@@ -31,9 +32,10 @@ void test1() {
     // printf("len = %zd\n", table->size);
     // printf("count = %d\n", count);
     Table_free(table);
+    return count;
 }
 
-void test2() {
+int test2() {
     std::unordered_map<int, int> table;
     srand(114514);
     int count = 0;
@@ -48,19 +50,23 @@ void test2() {
     }
     // printf("len = %zd\n", table.size());
     // printf("count = %d\n", count);
+    return count;
 }
 
-void timeit(void (*f)()) {
+void timeit(int (*f)()) {
     auto start = std::chrono::high_resolution_clock::now();
-    for (int j = 0; j < 100; j++) f();
+    int count;
+    for (int j = 0; j < 100; j++) count = f();
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
-    printf("time = %lf\n", duration);
+    printf("count = %d, time = %lf\n", count, duration);
 }
 
 int main() {
-    // timeit(test0);
+    timeit(test0);
     timeit(test1);
     timeit(test2);
+    // test1();
+    // test2();
     return 0;
 }
