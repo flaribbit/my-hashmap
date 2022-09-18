c9d6590c8b4a3c7d78f7968f1ab1804355e9c620
Rebuild <admin@rebuild.moe>
2022-09-17 20:58:43 -0400

    很好，通用性增强

--- table.h
+++ table.h
@@ -4,7 +4,11 @@
 #include <stdbool.h>
 
 typedef int TKey;
-typedef int TValue;
+typedef void* TValue;
+
+typedef TKey (*THashFunc)(TValue val);
+typedef int (*TCmpFunc)(TValue val1, TValue val2);
+typedef char* (*TToStrFunc)(TValue val);
 
 typedef struct Node {
     bool empty;
@@ -13,16 +17,21 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-typedef struct {
+typedef struct Table {
     size_t size;
     size_t capacity;
     Node *node;
     Node *lastfree;
+
+    THashFunc hashFunc;
+    TCmpFunc cmpFunc;
+    TToStrFunc toStrFunc;
 } Table;
 
-Table *Table_new();
-Node *Table_find(Table *, TKey key);
-Table *Table_del(Table *, TKey key);
-Table *Table_set(Table *, TKey key, TValue value);
+Table *Table_new(THashFunc hashFunc, TCmpFunc cmpFunc, TToStrFunc toStrFunc);
+Table *Table_add(Table *, TValue value);
+TValue Table_find(Table *, TValue val);
+Table *Table_del(Table *, TValue key);
+Table *Table_set(Table *, TValue value);
 void Table_free(Table *);
 void Table_debugprint(Table *);
--- test.cpp
+++ test.cpp
@@ -4,30 +4,96 @@ extern "C" {
 #include <unordered_map>
 #include <chrono>
 
-void test() {
-    Table *table = Table_new();
-    Table_set(table, 3, 114);
-    Table_set(table, 11, 514);
-    Table_set(table, 19, 514);
-    Table_set(table, 7, 514);
-    Table_set(table, 6, 514);
-    Table_set(table, 5, 514);
-    Table_set(table, 4, 514);
-    Table_del(table, 6);
-    Table_del(table, 5);
-    Table_del(table, 4);
-    Table_set(table, 5, 114);
-    Table_set(table, 6, 114);
-    Table_set(table, 7, 114);
-    Table_set(table, 8, 114);
-    Table_set(table, 9, 114);
-    Table_set(table, 10, 114);
+typedef struct {
+    int key;
+    int value;
+} item;
+
+int item_hash(void* v) {
+    item* i = (item*) v;
+    return i->key;
+}
+
+int item_cmp(void* v1, void* v2) {
+    item* i1 = (item*)v1;
+    item* i2 = (item*)v2;
+    return i1->key - i2->key;
+}
+
+char* item_tostr(void* it) {
+    item* i = (item*)it;
+    char* s = (char*)malloc(50);
+    sprintf(s, "item{key=%d, value=%d}", i->key, i->value);
+    return s;
+}
+
+void ex1() {
+    Table *table = Table_new(item_hash, item_cmp, item_tostr);
+    auto x = new item[13] {
+        {.key=3, .value=114},
+        {.key=11, .value=514},
+        {.key=19, .value=514},
+        {.key=7, .value=514},
+        {.key=6, .value=514},
+        {.key=5, .value=514},
+        {.key=4, .value=514},
+        {.key=5, .value=114},
+        {.key=6, .value=114},
+        {.key=7, .value=114},
+        {.key=8, .value=114},
+        {.key=9, .value=114},
+        {.key=10, .value=114},
+    };
+    Table_set(table, &x[0]);
+    Table_set(table, &x[1]);
+    Table_set(table, &x[2]);
+    Table_set(table, &x[3]);
+    Table_set(table, &x[4]);
+    Table_set(table, &x[5]);
+    Table_set(table, &x[6]);
+    Table_del(table, &x[4]);
+    Table_del(table, &x[5]);
+    Table_del(table, &x[6]);
+    Table_set(table, &x[7]);
+    Table_set(table, &x[8]);
+    Table_set(table, &x[9]);
+    Table_set(table, &x[10]);
+    Table_set(table, &x[11]);
+    Table_set(table, &x[12]);
     Table_debugprint(table);
-    printf("%p\n", Table_find(table, 2));
-    printf("%p\n", Table_find(table, 3));
-    printf("%p\n", Table_find(table, 11));
-    printf("%zd", table->size);
+//    printf("%p\n", Table_find(table, 2));
+//    printf("%p\n", Table_find(table, 3));
+//    printf("%p\n", Table_find(table, 11));
+//    printf("%zd", table->size);
     Table_free(table);
+    delete[] x;
+}
+
+void ex2() {
+    Table *table = Table_new(item_hash, item_cmp, item_tostr);
+    auto x = new item[3] {
+        {.key=0, .value=1},
+        {.key=8, .value=2},
+        {.key=16, .value=3},
+    };
+    Table_set(table, &x[0]);
+    Table_set(table, &x[1]);
+    Table_set(table, &x[2]);
+    Table_debugprint(table);
+    printf("------------------\n");
+    item finder = {.key = 0};
+    printf("%d\n", ((item*)Table_find(table, &finder))->value);
+    finder.key = 8;
+    printf("%d\n", ((item*)Table_find(table, &finder))->value);
+    finder.key = 16;
+    printf("%d\n", ((item*)Table_find(table, &finder))->value);
+    printf("------------------\n");
+    Table_del(table, &x[1]);
+    Table_del(table, &x[2]);
+    Table_del(table, &x[0]);
+    Table_debugprint(table);
+    Table_free(table);
+    delete[] x;
 }
 
 int test0() {
@@ -43,17 +109,17 @@ int test0() {
 }
 
 int test1() {
-    Table *table = Table_new();
+    Table *table = Table_new(item_hash, item_cmp, nullptr);
     srand(114514);
     int count = 0;
     for (int i = 0; i < 20000; i++) {
         int key = rand();
         int value = rand();
-        Table_set(table, key, value);
+//        Table_set(table, key, value);
     }
     for (int i = 0; i < 20000; i++) {
         int key = rand();
-        if (Table_find(table, key)) count++;
+//        if (Table_find(table, key)) count++;
     }
     // printf("len = %zd\n", table->size);
     // printf("count = %d\n", count);
@@ -89,10 +155,13 @@ void timeit(int (*f)()) {
 }
 
 int main() {
-    timeit(test0);
-    timeit(test1);
-    timeit(test2);
+//    timeit(test0);
+//    timeit(test1);
+//    timeit(test2);
     // test1();
     // test2();
+    ex1();
+    printf("=============\n");
+    ex2();
     return 0;
 }
